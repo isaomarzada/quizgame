aaa8f1e7c77d098e4cdd52ece3bb553a145fde41
Isa Omarzada <isaomarzada@yahoo.com>
2019-07-06 10:35:13 -0500

    changes

--- src/app.js
+++ src/app.js
@@ -1,10 +1,9 @@
 const express = require("express");
 const app = express();
-const appConfig = require("./config/main-config.js");
+//const appConfig = require("./config/main-config.js");
+//const routeConfig = require("./config/route-config.js");
 
-const routeConfig = require("./config/route-config.js");
-
-appConfig.init(app, express);
-routeConfig.init(app);
+//appConfig.init(app, express);
+//routeConfig.init(app);
 
 module.exports = app;
--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const app = require("./app");
 const http = require("http");
 const port = normalizePort(process.env.PORT || "3000");
+const fs = require('fs');
 app.set("port", port);
 
 const server = http.createServer(app);
