9ec5cf69c9d7d104740ef3c310a5245e24e0334e
Isa Omarzada <isaomarzada@yahoo.com>
2020-04-13 21:13:22 -0500

    Bug Fixed

--- src/quizgame1.js
+++ src/quizgame1.js
@@ -16,33 +16,19 @@ let availableQuestions = [];
 
 let questions = [];
 
-fetch('https://opentdb.com/api.php?amount=10&category=21&difficulty=medium&type=multiple')
+fetch("questions1.json")
   .then(res => {
     return res.json();
   })
   .then(loadedQuestions => {
-    console.log(loadedQuestions.results);
-    questions = loadedQuestions.results.map( loadedQuestion => {
-      const formattedQuestion = {
-        question: loadedQuestion.question
-      };
-
-      const answerChoices = [...loadedQuestion.incorrect_answers];
-      formattedQuestion.answer = Math.floor(Math.random() * 3) + 1;
-      answerChoices.splice(formattedQuestion.answer - 1, 0,
-      loadedQuestion.correct_answer);
-
-      answerChoices.forEach((choice, index) => {
-        formattedQuestion["choice" + (index + 1)] = choice;
-      });
-
-      return formattedQuestion;
-    })
+    console.log(loadedQuestions);
+    questions = loadedQuestions;
     startGame();
   })
   .catch(err => {
-    console.log('There has been an error!');
+    console.log(err);
   });
+
 
 const CORRECT_BONUS = 10;
 const MAX_QUESTIONS = 10;
@@ -80,8 +66,11 @@ getNewQuestion = () => {
     if (scoreText.innerText >= '70') {
       endText.innerText = 'You Passed! Your score was ' + scoreText.innerText + '%';
     }
+    else if (scoreText.innerText == '100') {
+      endText.innerText = 'You Passed! Your score was ' + scoreText.innerText + '%';
+    }
     else {
-      endText.innerText = 'You Failed. Your score was ' + scoreText.innerText + '%';
+      endText.innerText = 'You failed. Your score was ' + scoreText.innerText + '%';
     }
 
   }
